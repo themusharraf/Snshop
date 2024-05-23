7c3c457f756ac38152ccd32bfea185045d2f66a5
msi <meibrohimov@gmail.com>
2024-05-23 10:28:00 +0500

    add new change

--- root/settings.py
+++ root/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,7 +94,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- root/urls.py
+++ root/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib import admin
+
+from .settings import STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.urls'))
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('apps.urls'))
+
+] + static(STATIC_URL, document_root=STATIC_ROOT) + static(MEDIA_URL, document_root=MEDIA_ROOT)
